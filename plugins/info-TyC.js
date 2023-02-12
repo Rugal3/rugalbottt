e527844b111709ebd1e59eff47949f665dafb7c6
JhonatanPpp <119618637+JhonatanPpp@users.noreply.github.com>
2023-02-11 21:06:58 -0500

    Update info-TyC.js

--- plugins/info-TyC.js
+++ plugins/info-TyC.js
@@ -49,7 +49,6 @@ _- Si en algún momento recibe un mensaje del numero del Bot y no es un comando 
 _- Si todavía tienes alguna duda o reclamo de lo mencionado aqui o referente al Bot, contáctame a mi numero personal al cual estaré respondiendo lo antes posible para brindarte una solución y/o hacerme responsable de alguna reclamación_ 
 
 > wa.me/5219992095479
-> Bruno Sobrino 
-> Correo electronico: theshadowbrokers133@gmailcom
+> Lytz Oficial 
 
-*∆ THE MYSTIC - BOT - MD ∆*`
+*∆ Lytz - BOT - MD ∆*`
