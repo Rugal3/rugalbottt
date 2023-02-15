c76a8a9c88e4c308324f99aa4ec1303db87f7340
JhonatanPpp <119618637+JhonatanPpp@users.noreply.github.com>
2023-02-15 14:53:52 -0500

    Update config.js

--- config.js
+++ config.js
@@ -420,9 +420,9 @@ global.mes = d.toLocaleDateString('es', { month: 'long' })
 global.año = d.toLocaleDateString('es', { year: 'numeric' })
 global.tiempo = d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })
 //*****************************
-global.wm2 = `▸ ${dia} ${fecha}\n▸ 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝`
-global.gt = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
-global.mysticbot = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
+global.wm2 = `▸ ${dia} ${fecha}\n▸ @𝐥𝐲𝐭𝐳_𝐨𝐟𝐢𝐜𝐢𝐚𝐥`
+global.gt = '@𝐥𝐲𝐭𝐳_𝐨𝐟𝐢𝐜𝐢𝐚𝐥'
+global.mysticbot = '@𝐥𝐲𝐭𝐳_𝐨𝐟𝐢𝐜𝐢𝐚𝐥'
 global.md = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'
 global.mysticbot = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'
 global.waitt = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
